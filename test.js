4a0f3aaa2857399e7a4a44e957d7a796e9812b5a
Kazuma Hatta <tori31001@gmail.com>
2016-01-01 00:08:19 +0900

    change package.json

--- test.js
+++ test.js
@@ -5,7 +5,7 @@
 		abcio = require('umabcio');
 
 	function loadtest() {
-		var file = "D:\\alembic\\alembic_file.abc",
+		var file = "alembic_octopus.abc",
 			path_list,
 			i;
 		console.log("hogehoge");
@@ -15,9 +15,11 @@
 		console.log(path_list);
 		console.log(abcio.get_total_time(file));
 		abcio.set_time(file, 3000);
+		/*
 		for (i = 0; i < path_list.length; i = i + 1) {
 			console.log(abcio.get_mesh(file, path_list[i]));
 		}
+		*/
 	}
 	loadtest();
 }());
